e01b6e8df4763de16488b3a51c6b02017933e6e3
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2025-12-02 21:11:08 +0300

    edit files

--- basics-computer-science/laba1/step_3.java
+++ basics-computer-science/laba1/step_3.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // вариант 1
+        int x = 2;
+        int y = 6;
 
-        int x = 2;   // значение из варианта 1
-        int y = 6;   // значение из варианта 1
-
-        int result = 2 * x + 3 * y; // само выражение
+        int result = 2 * x + 3 * y;
 
         System.out.println("Результат выражения 2x + 3y = " + result);
 
-        // Доп — пример цикла: сумма от 1 до 100
+        // пример из файла
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             sum = sum + i;
--- basics-computer-science/laba9/Main.java
+++ basics-computer-science/laba9/Main.java
@@ -3,7 +3,6 @@ import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
-
         int[][] matrix = {
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
